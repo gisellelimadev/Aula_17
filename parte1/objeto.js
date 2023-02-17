c79ee19ee6f3463f0b1ee1fb4a8f0b71d6cad76a
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-02-17 11:05:14 -0300

    aula17

---
+++ parte1/objeto.js
@@ -0,0 +1,31 @@
+let pessoa = {
+    altura: 1.77,
+    pele: {
+        tom:"clara",
+        tipo: "oleosa"
+    },
+
+    olhos: "castanhos",
+    cabelo: "preto",
+    idade: 32
+}
+
+let produto ={
+    nome: "arroz",
+    preço: 4.69,
+    qnt: 3
+}
+let lista = [
+    { 
+        nome: "arroz",
+        preco: 4.69,
+        qtd: 3
+    },
+    {
+    nome:"cx. ovos",
+    preco: 17.00,
+    qtd: 1,
+}
+];
+
+document.write(pessoa.idade);
---
+++ parte2/eventos.js
@@ -0,0 +1,10 @@
+// let nota1 = 10;
+// let nota2 = 9;
+// let nota3 = 8.5;
+// let nota4 = 9.8;
+
+let notas = [10,9,8.5,9.8,];
+
+let total = notas[0] + notas[1] + notas[2] + notas[3];
+
+alert(total/4);
