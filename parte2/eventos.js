3b1c764558a052102af0ce5608000756d607141c
Bruno-GSilva <gisellenanda-@hotmail.com>
2023-02-17 11:12:42 -0300

    aula17

--- parte2/eventos.js
+++ parte2/eventos.js
@@ -3,8 +3,8 @@
 // let nota3 = 8.5;
 // let nota4 = 9.8;
 
-let notas = [10,9,8.5,9.8,];
+let notasG = [10,9,8.5,9.8,];
 
-let total = notas[0] + notas[1] + notas[2] + notas[3];
+let totalG = notasG[0] + notasG[1] + notasG[2] + notasG[3];
 
 alert(total/4);
